1352ac78d395576b15d60948d4029b14e7bd6b8e
© Jakob 3301™ <danielsen.jakob@gmail.com>
2025-10-15 16:26:18 +0200

    code rework
    now does the operation based on "0 <= remainder < divisor" instead of "cheating" by testing the answer with multiplication
    
    Can now take negative dividend but not negative divisor

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
-import  javax.swing.*; // TODO: ABILITY TO TAKE IN NEGATIVE INTEGERS
+import  javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         int dividend = Integer.parseInt(JOptionPane.showInputDialog("What integer are you dividing?"));
         int divisor = Integer.parseInt(JOptionPane.showInputDialog("What integer are you dividing it with?"));
         int quotient = 0;
-        int rest = 0;
-        if (divisor != 0) { 
-            while (!(rest+(quotient*divisor)==dividend)){ // exits loop if the equation is solved
-                if((quotient+1)*divisor>dividend){ // checking for overflow
-                    rest = dividend-divisor*quotient;
+        int remainder = dividend < 0 ? -dividend : dividend; // Setting remainder as absolute value of dividend, this compacts the code if dividend < 0
+        
+        if (divisor != 0) { // cheks if the user is a troll
+            while (remainder >= divisor){ // using 0 <= remainder < divisor, to find out if the equation is done
+                   remainder -= divisor;
+                   quotient++;
+            }
+            if (dividend < 0) { // correcting values if the dividend is negative
+                if (remainder == 0) {
+                    quotient = -quotient;
                 } else {
-                    quotient++;
+                    quotient = -(quotient + 1);
+                    remainder = divisor - remainder;
                 }
             }
-            JOptionPane.showMessageDialog(null,dividend + " = " + divisor + " * " + quotient+ " + " + rest+"\n" + dividend + "/" + divisor + " = " + quotient + " with " + rest +" as rest." );
+            JOptionPane.showMessageDialog(null,dividend + " = " + divisor + " * " + quotient+ " + " + remainder +"\n" + dividend + "/" + divisor + " = " + quotient + " with " + remainder +" as remainder." );
         } else {
             // if trying to divide by zero
             ImageIcon icon = new ImageIcon(Main.class.getResource("/resources/really.jpg"));
